dfa904642cac3395c6de4e59730b67082e2b6d37
Eddy Ding <edwarding04@gmail.com>
2023-07-13 17:09:29 -0400

    Complete 3.8

--- phonebook/index.js
+++ phonebook/index.js
@@ -2,7 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 app.use(express.json());
-app.use(morgan());
+app.use(morgan(function (tokens, req, res) {
+  const body = req.body;
+  let content = null;
+  if(body) content = JSON.stringify(body);
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    content
+  ].join(' ')
+}));
 let persons = [
     { 
       "id": 1,
